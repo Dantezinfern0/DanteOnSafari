92d592949f8cc1da9d8973a953073a1c19c5d4bc
dantezinfern0 <danteharasz@gmail.com>
2019-08-27 20:31:29 -0400

    commmit

--- Program.cs
+++ Program.cs
@@ -15,18 +15,42 @@ namespace DanteOnSafari
         input = Console.ReadLine();
         if (input == "add")
         {
-            Console.WriteLine("Type in: Species, Location");
-            input = Console.ReadLine();
+          Console.WriteLine("Type in: Species, How Many Times Seen, and  Location");
+          input = Console.ReadLine();
           var data = input.Split(',');
           var newAnimal = new Animal
           {
             Species = data[0],
-            LocationOfLastSeen = data[1]
+            CountOfTimesSeen = int.Parse(data[1]),
+            LocationOfLastSeen = data[2]
           };
-          // db.SafariVacation.Add(newAnimal);
+          db.SeenAnimals.Add(newAnimal);
           db.SaveChanges();
           Console.WriteLine($"Successfully Saved {newAnimal.Species}");
+        }
+        else if (input == "list")
+        {
+          Console.WriteLine("Here are your records");
+          var allAnimals = db.SeenAnimals;
+          foreach (var data in allAnimals)
+          {
+            Console.WriteLine($"{data.Species} seen in the {data.LocationOfLastSeen}, {data.CountOfTimesSeen} times");
+          }
+        }
+        else if (input == "update")
+        {
 
+        }
+        else if (input == "remove")
+        {
+          Console.WriteLine("Please Type in the Name of the Species you would like to Remove");
+          input = Console.ReadLine();
+          var dupe = db.SeenAnimals.FirstOrDefault(f => f.Species == input);
+          if (dupe != null)
+          {
+            db.SeenAnimals.Remove(dupe);
+            db.SaveChanges();
+          }
         }
       }
     }
